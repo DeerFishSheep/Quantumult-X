e4801cba3939cae6e04585c3a45f25d48ec47c3c
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2022-10-17 03:04:48 +0800

    Create XunLeiHuaZhi.js

---
+++ XunLeiHuaZhi.js
@@ -0,0 +1,26 @@
+/*
+‼️规则完全免费，仅供学习交流，🈲️商业用途
+✅迅雷解锁默认原画及部分超级会员功能
+By: 白了个鹿🦌
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+obj.medias[0].need_more_quota = false;
+obj.medias[1].need_more_quota = false;
+obj.medias[1].is_default = false;
+obj.medias[2].is_default = false;
+obj.medias[3].is_default = false;
+obj.medias[0].is_default = true;
+obj.medias[0].link = obj.links["application/octet-stream"];
+obj.medias[0].vip_types = [
+
+      ];
+obj.medias[1].vip_types = [
+
+      ];
+
+body = JSON.stringify(obj);
+$done({body});
