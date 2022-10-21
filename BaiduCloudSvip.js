fe91baaec7c7bcac9167a39bc72eb4f344f23865
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2022-10-21 20:24:12 +0800

    Update BaiduCloudSvip.js

--- BaiduCloudSvip.js
+++ BaiduCloudSvip.js
@@ -41,7 +41,7 @@ if ($response.body) {
                 "buy_time": 0,
                 "product_id": "1",
                 "auto_upgrade_to_svip": 0,
-                "end_time": 4102415999,
+                "end_time": 1708950214,
                 "cluster": "vip",
                 "detail_cluster": "svip",
                 "status": 0
