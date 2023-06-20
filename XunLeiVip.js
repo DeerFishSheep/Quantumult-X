f6bfc05c4a8080b5bd08ffb6f70c12ccf81b1504
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2023-06-20 21:32:36 +0800

    Update XunLeiVip.js

--- XunLeiVip.js
+++ XunLeiVip.js
@@ -10,18 +10,44 @@ var obj = JSON.parse(body);
 
 obj.vipList = [
     {
-      "isAutoDeduct" : "1",
-      "isYear" : "1",
-      "payId" : "1422461465",
+      "daily" : "9999",
+      "payId" : "0",
+      "lastPay" : "1686211943",
       "isVip" : "1",
-      "vipLevel" : "8",
-      "register" : "20220520",
-      "expireDate" : "20991231",
-      "payName" : "白了个鹿",
-      "vipDayGrow" : "9999",
+      "icon" : {
+        "general" : "https://xluser-ssl.xunlei.com/v1/file/icon/level/svip/normal_a/svip_level8_normal.png",
+        "small" : "https://xluser-ssl.xunlei.com/v1/file/icon/level/svip/normal_b/svip_lever8_normal.png"
+      },
+      "isNew" : "0",
+      "vipDayGrow" : "20",
       "vipGrow" : "99999",
+      "isYear" : "1",
+      "isRemind" : "0",
       "vasid" : "2",
-      "vasType" : "5"
+      "suplusDay" : "99999",
+      "isAutoDeduct" : "0",
+      "vasType" : "5",
+      "payName" : "---",
+      "register" : "20220520",
+      "detail" : [
+        {
+          "isvip" : 1,
+          "vas_type" : "5",
+          "end" : "2099-12-31T16:12:23+08:00",
+          "surplus_day" : 21,
+          "start" : "2023-06-08T16:12:23+08:00"
+        },
+        {
+          "isvip" : 1,
+          "vas_type" : "3",
+          "end" : "2099-12-31T16:11:43+08:00",
+          "surplus_day" : 1,
+          "start" : "2023-07-11T16:12:23+08:00"
+        }
+      ],
+      "expireDate" : "20991231",
+      "expireTime" : "2099-12-31T16:11:43+08:00",
+      "vipLevel" : "8"
     }
   ];
 
