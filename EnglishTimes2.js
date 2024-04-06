133900f06d239632c3fcc008eb25058b4df720dc
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2024-04-06 19:56:50 +0800

    Update EnglishTimes2.js

--- EnglishTimes2.js
+++ EnglishTimes2.js
@@ -5,17 +5,5 @@ By: 白了个鹿🦌
 */
 
 ^https://uploadpro.hellotalk8.com/et_login_config_querier/query url script-response-body https://raw.githubusercontent.com/DeerFishSheep/Quantumult-X/main/EnglishTimes.js
+
 hostname = uploadpro.hellotalk8.com
-
-let url = $request.url;
-let res = $response.body;
-
-if (url.indexOf("/et_login_config_querier/query") !== -1) {
-    let obj = JSON.parse(res);
-    obj.data.switch.free_read_time = 600240;
-    obj.data.free_read_count = 9999;
-    res = JSON.stringify(obj);
-    $done({body: res});
-} else {
-    $done({});
-}
