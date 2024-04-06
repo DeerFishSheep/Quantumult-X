1cffe4c7161a208283544cb139ebcbce486e8dc2
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2024-04-06 21:18:41 +0800

    Update EnglishTimes.js

--- EnglishTimes.js
+++ EnglishTimes.js
@@ -4,7 +4,7 @@
 By: 白了个鹿🦌
 */
 
-^https://uploadpro.hellotalk8.com/et_login_config_querier/query url script-response-body https://raw.githubusercontent.com/DeerFishSheep/Quantumult-X/main/EnglishTimes.js
+^https:\/\/uploadpro\.hellotalk8\.com\/et\_login\_config\_querier\/query url script-response-body https://raw.githubusercontent.com/DeerFishSheep/Quantumult-X/main/EnglishTimes.js
 hostname = uploadpro.hellotalk8.com
 
 let url = $request.url;
