abd1a051a0a78b08af42dd333e8af37c869d88c8
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2024-04-06 20:18:07 +0800

    Update EnglishTimes.js

--- EnglishTimes.js
+++ EnglishTimes.js
@@ -6,15 +6,11 @@ By: 白了个鹿🦌
 
 hostname = uploadpro.hellotalk8.com
 
-let url = $request.url;
+let url = https://uploadpro.hellotalk8.com/et_login_config_querier/query;
 let res = $response.body;
+let obj = JSON.parse(res);
 
-if (url.indexOf("/et_login_config_querier/query") !== -1) {
-    let obj = JSON.parse(res);
-    obj.data.switch.free_read_time = 600240;
-    obj.data.free_read_count = 9999;
-    res = JSON.stringify(obj);
-    $done({body: res});
-} else {
-    $done({});
-}
+obj.data.switch.free_read_time = 600240;
+obj.data.free_read_count = 9999;
+res = JSON.stringify(obj);
+$done({body: res});
