dacde18f3c25bfff91eed741cb3d496f70f94f0a
白鹿 <105504128+DeerFishSheep@users.noreply.github.com>
2024-04-06 19:24:08 +0800

    Create EnglishTimes2.js

---
+++ EnglishTimes2.js
@@ -0,0 +1,4 @@
+[rewrite_local]
+^https://uploadpro.hellotalk8.com/et_login_config_querier/query url script-response-body https://raw.githubusercontent.com/DeerFishSheep/Quantumult-X/main/EnglishTimes.js
+[mitm]
+hostname = uploadpro.hellotalk8.com
